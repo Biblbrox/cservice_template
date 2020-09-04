0419b1a9619fe497860b3deefe91cafc6e043aa5
Dmitrii Leliuhin <dleliuhin@gmail.com>
2020-09-04 21:23:56 +0300

    Fix. All classes parsing.

--- src/main.cpp
+++ src/main.cpp
@@ -51,6 +51,8 @@ int main( int argc, char **argv )
         config.logs.setup();
     }
 
+    int *i;
+
     //-----------------------------------------------------------------------------------
 
     // Link signals -> slots
