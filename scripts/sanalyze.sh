ea9621314f7ad3ff140d4f0209797ef797ccd3e4
Dmitrii Leliuhin <dleliuhin@gmail.com>
2020-09-07 15:14:46 +0300

    Fix. All classes analyze.

--- scripts/sanalyze.sh
+++ scripts/sanalyze.sh
@@ -16,4 +16,5 @@ include_args="$(cat ./compile_commands.json | grep command | sed "s/-I/\n-I/g" |
 
 checks=$(cat ../cfg/sanalyzer.cfg)
 
-clang-tidy -config="$checks" $main -p=. --header-filter="src/*" -- $include_args -I/usr/include/x86_64-linux-gnu/c++/8 -I/usr/include/c++/8 -std=c++17 -fexceptions -fcxx-exceptions 2>/dev/null
+clang-tidy -config="$checks" $main -p=. --header-filter="src/*" -- $include_args -I/usr/include/c++/8 -I/usr/include/x86_64-linux-gnu/c++/8 -pthread -std=c++11 -fexception -fPIC -std=gnu++17 2>/dev/null
+
